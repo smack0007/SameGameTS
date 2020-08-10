db222a76225943681bc9c6be3121725350834e18
smack0007 <zachary.snow@gmail.com>
2020-08-10 21:31:24 +0200

    Added @types/node to ext.

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@
 PUSHD %~dp0
 
 ECHO Building...
-CALL tsc .\scripts\build.ts && CALL node .\scripts\build.js
+CALL tsc --project .\scripts\tsconfig.json && CALL node .\scripts\build.js
 
 ECHO Done.
 POPD
