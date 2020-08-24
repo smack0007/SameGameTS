7783a8334b53e16c142fa427cc015542937311dc
smack0007 <zachary.snow@gmail.com>
2020-08-24 21:06:49 +0200

    Replace build.ts with cpy.

--- scripts/environment.ts
+++
@@ -1,9 +0,0 @@
-import * as path from 'path';
-
-export const respositoryRootDirectory = path.join(__dirname, "..");
-
-export function outputEnvironmentVariables(output: (x: string) => void = console.info) {
-    output('\n=== Environment ===');
-    output(`respositoryRootDirectory = '${respositoryRootDirectory}'`);
-    output('======\n');
-}
